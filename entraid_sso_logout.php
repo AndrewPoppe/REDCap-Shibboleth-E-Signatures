5dfc26b84087d53d8a75ee6af2196c8da5d5498c
AndrewPoppe <AndrewBPoppe@gmail.com>
2024-06-24 13:39:56 -0400

    Debug

--- entraid_sso_logout.php
+++ entraid_sso_logout.php
@@ -4,4 +4,9 @@ namespace YaleREDCap\EntraIdAuthenticator;
 
 /** @var EntraIdAuthenticator $module */
 
-$module->log('logout request', [ 'get' => json_encode($_GET, JSON_PRETTY_PRINT) ]);
+$module->log('logout request', [ 
+    'get' => json_encode($_GET, JSON_PRETTY_PRINT),
+    'post' => json_encode($_POST, JSON_PRETTY_PRINT),
+    'cookie' => json_encode($_COOKIE, JSON_PRETTY_PRINT),
+    'session' => json_encode($_SESSION, JSON_PRETTY_PRINT)
+]);
